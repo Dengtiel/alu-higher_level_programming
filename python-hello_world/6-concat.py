6d59235b31c50499785b6bbcaf4c81cacd5a6e65
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-21 23:31:09 +0200

    Add 6-concat.py to print concatenated Welcome message

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Battery Street"
-str3 = str1 + " " + str2
-print(f"{str3}!")
+str1 = "Holberton"
+str2 = "School"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"Welcome to {str1}!")
