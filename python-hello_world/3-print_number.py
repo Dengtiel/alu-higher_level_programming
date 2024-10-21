a4db9692acf7cadf043305cc0d576d1950eff847
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-21 23:52:26 +0200

    Complete the code to print the number and message

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number = 98
-print(f"{number} Battery street")
+number = "98"  # Intentionally using a string instead of an integer
+print(f"{number:d} Battery street")  # This will raise the ValueError
