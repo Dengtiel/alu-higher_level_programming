858b6dcb38d7f6a870d9f674dbd1fabc1e3bf84d
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-22 01:38:29 +0200

    Complete 8-concat_edges.py to print "object-oriented programming with Python" using string indexing.

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# Output: object-oriented programming with Python
-print(f"{str[39:67]} with {str[0:6]}")
+print(f"{str[39:67]} with {str[0:6]}")  
+print("")
