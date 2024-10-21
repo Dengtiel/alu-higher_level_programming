6816c000a801524ce3b5735c181ed3d7b45d56d6
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-21 23:14:54 +0200

    Add 6-concat.py to print concatenated Welcome message

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os
 str1 = "Welcome to"
-str2 = "Battery Street" if os.getenv('SCHOOL') == 'Battery' else "Holberton School"
-print(f"{str1} {str2}!")
+str2 = "Battery Street"
+str3 = str1 + " " + str2
+print(f"{str3}!")
