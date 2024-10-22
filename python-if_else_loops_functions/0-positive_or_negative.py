ae84a5d8600a42e38ffd1b665c9167d66df36ee9
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-22 18:52:12 +0200

    Add program to determine if a number is positive, negative, or zero

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10, 10)  # This line generates a random integer between -10 and 10
+# This line generates a random integer between -10 and 10
+number = random.randint(-10, 10)
 
 # Your code goes here. Please remove the comment before the following code.
 if number > 0:
