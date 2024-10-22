f7340a9ef45125a32115408d730c4a9235e50329
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-22 18:16:48 +0200

    Updated 3-print_number.py to handle type errors and adhere to requirements

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 number = 98
 
-try:
-    print(f"{number} Battery street")
-except ValueError:
-    print(f"ValueError: Unknown format code 'd' for object of type '{type(number).__name__}'")
+print(f"{number} Battery street")
