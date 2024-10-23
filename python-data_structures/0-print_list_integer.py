7ae40f75f3dd001b8c4d1d65f7f228112a25f2ec
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-23 17:32:39 +0200

    Implement function to print a list of integers

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     """Prints all integers of a list, one per line."""
     for num in my_list:
-        print("{}".format(num))
+        print("{:d}".format(num))
