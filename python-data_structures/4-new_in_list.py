1ff355aa97d4088d6a5d2dac6f71d5c4479fdc56
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-23 18:00:40 +0200

    Add function to replace an element in a copy of a list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Replaces an element in a list at a specific position without modifying the original list."""
+    """Replaces an element in a list at a specific position 
+    without modifying the original list."""
+    
     if idx < 0 or idx >= len(my_list):
-        return my_list.copy()  # Return a copy of the original list if idx is out of range
+        return my_list.copy()  # Return a copy if idx is out of range
 
     # Create a copy of the original list
     new_list = my_list.copy()
