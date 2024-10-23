42f485d4a335386f9578b525b04388a94c326840
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-23 18:36:59 +0200

    Add function to add two tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,5 +4,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Ensure both tuples have at least 2 elements
     tuple_a += (0, 0)
     tuple_b += (0, 0)
-    # Return a tuple with the sum of the first and second elements of both tuples
+    # Return a tuple with the sum of the first and second elements
+    # of both tuples
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
