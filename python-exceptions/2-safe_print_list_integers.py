8561b254c4380fc9fdfed82918e590f8a668a17c
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-27 23:53:55 +0200

    Allow IndexError to propagate in safe_print_list_integers function

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
-        # Try to print the integer; if it 
-        fails, it will raise an exception
+        # Try to print the integer; if it fails, it will raise an exception
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
