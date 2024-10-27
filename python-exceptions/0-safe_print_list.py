35a19aad452634e49e571a9e5888d973819a9cd6
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-27 23:14:14 +0200

    Add function safe_print_list to print elements of a list with exception handling

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0  # To count the number of elements printed
+    count = 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-            count += 1  # Increment count for each successfully printed element
+            count += 1
     except IndexError:
-        pass  # Ignore IndexError if x is greater than the list length
-    print("")  # Print new line after all elements
-    return count  # Return the actual number of elements printed
-
+        pass
+    print("")
+    return count
---
+++ python-exceptions/README.md
@@ -0,0 +1,4 @@
+# Python Exceptions Project
+
+## Project Overview
+This project is a set of exercises focused on handling exceptions in Python. The exercises require implementing functions that handle errors effectively, allowing the code to execute without crashing even if unexpected inputs occur.
