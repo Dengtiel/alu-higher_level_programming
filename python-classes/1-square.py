df5237bb950df6237b09a396859ae232a03af0fd
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-28 16:22:12 +0200

    Implement Square class with private size attribute

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,8 +4,6 @@
 
 class Square:
     """A class that defines a square by its size."""
-
-
     def __init__(self, size):
         """Instantiation with size."""
         self.__size = size  # Private instance attribute
