a4dad1e3a730f9cebd6668b1537ea5b728500443
Dengtiel <dengtiel@Dengtiel-Junior>
2024-10-29 18:40:12 +0200

    Implement Rectangle class with eval-compatible __repr__ method

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -56,12 +56,8 @@ class Rectangle:
         """Returns a string representation of the rectangle with '#' characters."""
         if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join(
-            "#" * self.width for _ in range(self.height)
-        )
+        return "\n".join("#" * self.width for _ in range(self.height))
 
     def __repr__(self):
-        """Returns a string representation that can recreate the rectangle instance."""
-        return (
-            f"Rectangle({self.width}, {self.height})"
-        )
+        """Returns a string representation to recreate the rectangle instance."""
+        return f"Rectangle({self.width}, {self.height})"
