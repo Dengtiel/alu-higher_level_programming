167b0cb50ab5ac2ca2126f0bc96179033fbef564
Dengtiel <dengtiel@Dengtiel-Junior>
2024-11-05 23:13:42 +0200

    Add MyList class with print_sorted method and test file 1-my_list.txt

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+This module contains a class MyList which is a subclass of the built-in list class.
+It includes an additional method to print the list in sorted order without modifying the original list.
+"""
 
 
 class MyList(list):
-    """A list subclass with additional functionality to print the list in sorted order."""
+    """A list subclass with a method to print the list in sorted order."""
 
     def print_sorted(self):
-        """Prints the list elements in ascending order without modifying the original list."""
+        """Prints the list in ascending order without modifying the original list."""
         print(sorted(self))
