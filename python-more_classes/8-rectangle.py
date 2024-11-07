47ef7118e4e91be84ef40303aa08df21bee3289a
Dengtiel <dengtiel@Dengtiel-Junior>
2024-11-07 19:12:15 +0200

    error of too much characters fix

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     Attributes:
         number_of_instances (int): number of Rectangle instances created.
         print_symbol (any): The symbol used for string depicting the rectangle.
-  """ 
+    """
     number_of_instances = 0
     print_symbol = "#"
 
@@ -34,8 +34,8 @@ class Rectangle:
 
         Args:
             value (int): The width of the rectangle.
-        
-       Raises:
+
+        Raises:
             TypeError: If width is not an integer.
             ValueError: If width is less than 0.
         """
@@ -56,7 +56,7 @@ class Rectangle:
 
         Args:
             value (int): The height of the rectangle.
-        
+
         Raises:
             TypeError: If height is not an integer.
             ValueError: If height is less than 0.
@@ -100,32 +100,10 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         """Return the rectangle with the larger area.
 
-      Args:
+        Args:
             rect_1 (Rectangle): The first rectangle.
             rect_2 (Rectangle): The second rectangle.
-        
-     Raises:
-            TypeError: If rect_1 or rect_2 is not an instance of Rectangle.
-        
-    Returns:
-            Rectangle: with the greater area, or rect_1 if both areas are equal.
         """
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        """Class returns new Rectangle instance with width == height == size.
-
-      Args:
-            size (int): The size of the square.
-        
-        Returns:
-            Rectangle: A new rectangle instance with equal width and height.
-        """
-        return cls(size, size)
