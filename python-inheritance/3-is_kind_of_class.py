bf6e342be1298d3a929cfcc359d965796921824f
Dengtiel <dengtiel@Dengtiel-Junior>
2024-11-07 22:48:10 +0200

    error of too much characters fix

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,18 +5,18 @@
 def is_kind_of_class(obj, a_class):
     """Returns True if the object is an
     instance of, or
-    if the object is an instance of a class
+    if the object is an instance of a class\
     that inherited
     from, the specified class; otherwise False.
     Args:
-        obj: The object to check.
+        obj: The object to check.\
         a_class: The class to match the 
         object against.
 
     Returns:
-        bool: True if `obj` is an instance
+        bool: True if `obj` is an instance\
         of `a_class` or
-        an instance of a subclass of
+        an instance of a subclass of\
         `a_class`; otherwise False.
     """
     return isinstance(obj, a_class)
