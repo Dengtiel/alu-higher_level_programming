3fad9e93dd50473d6e92ecdf2ec2a7e2f454d6e8
Dengtiel <dengtiel@Dengtiel-Junior>
2024-11-07 22:51:06 +0200

    error of too much characters fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,5 +6,6 @@ of a specified class.
 
 
 def is_same_class(obj, a_class):
-    """Returns True if the object is exactly an instance of the specified class."""
+    """Returns True if the object is exactly an instance\
+            of the specified class."""
     return type(obj) == a_class
