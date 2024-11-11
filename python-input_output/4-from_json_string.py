c7dc9f47342a822e26e8e33cf25dd4bdf5a327ff
Dengtiel <dengtiel@Dengtiel-Junior>
2024-11-11 18:08:54 +0200

    Add function to parse JSON string into Python object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module provides a function to parse a JSON\
+        string into a Python object.
+"""
+
+
+import json
+
+def from_json_string(my_str):
+    """
+    Returns a Python object represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to be converted.
+
+    Returns:
+        object: The Python data structure represented\
+                by the JSON string.
+    """
+    return json.loads(my_str)
