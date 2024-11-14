f665957baaaa3d42c7d183083d9132258c3e2d0f
Dengtiel <dengtiel@Dengtiel-Junior>
2024-11-15 00:26:53 +0200

    Add Student class with to_json method supporting attribute filtering

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
-class Student:
-    """Class to define a student with first_name, last_name, and age."""
+"""Defines a class Student."""
 
+
+class Student:
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initialize the student with first_name, last_name, and age.
-
+        """Initialize a new Student.
         Args:
-            first_name (str): The student's first name.
-            last_name (str): The student's last name.
-            age (int): The student's age.
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retrieve a dictionary representation of the Student instance, optionally filtering 
-        by attributes in the attrs list.
-
-        If attrs is provided, only the attributes that appear in the list will be included 
-        in the returned dictionary. Otherwise, all attributes of the Student instance will 
-        be included.
-
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
         Args:
-            attrs (list, optional): A list of attribute names to include in the dictionary.
-
-        Returns:
-            dict: The dictionary representation of the student, with the specified attributes.
+            attrs (list): (Optional) The attributes to represent.
         """
-        if attrs is None:
-            return self.__dict__
-        else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
-
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
