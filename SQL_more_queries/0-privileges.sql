12ec2aeb3a8b6cecd722f90edd3526d5ef0db6ca
Dengtiel <dengtiel@Dengtiel-Junior>
2024-11-16 01:19:46 +0200

    Added SQL script for user privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,15 @@
--- Script that lists all privileges of the MySQL users
--- Query to list all privileges (GRANT) of the MySQL users
+-- Create the users (if not already created)
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Grant privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Grant privileges to user_0d_2
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+-- Show the privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show the privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
