a21c9efca6eb272d41d391bc4e11b985dfa44344
Dengtiel <dengtiel@Dengtiel-Junior>
2024-11-26 01:06:17 +0200

    Fix URL to fetch http://0.0.0.0:5050/status and update response handling

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-
-Fetches https://alu-intranet.hbtn.io/status
-and displays information about the response.
+Fetches http://0.0.0.0:5050/status and displays information about the response.
 """
 
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    # You can update the URL here to either the local server or the remote one
+    url = "http://0.0.0.0:5050/status"  # Update to the correct URL
 
     with urllib.request.urlopen(url) as response:
         body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
 
+    # Display the body response
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
