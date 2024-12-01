78961d6891700786e940c14f843dac8a2250dfe6
Dengtiel <dengtiel@Dengtiel-Junior>
2024-12-01 17:10:36 +0200

    python-network_1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,27 +1,10 @@
 #!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status."""
 import requests
 
 
-"""
-This script fetches the status from the URL https://alu-intranet.hbtn.io/status
-and prints the response's type and content in a specific format.
-
-Usage:
-    ./4-hbtn_status.py
-
-Dependencies:
-    - requests: To make an HTTP GET request to the URL.
-
-Output:
-    - type: The type of the response body (should be <class 'str'>).
-    - content: The content of the response body (should be 'OK').
-"""
-
 if __name__ == "__main__":
-    # Fetch the URL
-    r = requests.get("https://alu-intranet.hbtn.io/status")
-
-    # Print the body response in the specified format
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print(f"\t- type: {type(r.text)}")
-    print(f"\t- content: {r.text}")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
